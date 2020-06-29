eea7b6c8f72bbd8d304277789e8136f4f360b083
username <email>
2020-06-28 23:33:37 -0700

    moved mine_at_model to parent dir.

--- mine_at_model.py
+++ mine_at_model.py
@@ -92,12 +92,12 @@ def get_cifar10(batch_size):
         # Normalization messes with l-inf bounds.
     ])
 
-    trainset = torchvision.datasets.CIFAR10(root='../data', train=True, download=True, transform=transform_train)
+    trainset = torchvision.datasets.CIFAR10(root='data', train=True, download=True, transform=transform_train)
     trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=False, drop_last=True)
     transform_test = transforms.Compose([
         transforms.ToTensor()
     ])
-    testset = torchvision.datasets.CIFAR10(root='../data', train=False, download=True, transform=transform_test)
+    testset = torchvision.datasets.CIFAR10(root='data', train=False, download=True, transform=transform_test)
     testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size, shuffle=False, num_workers=2)
 
     return trainloader, testloader
